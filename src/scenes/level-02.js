0b3ded5db4392ecd52f82b73a2232081d6839ea0
Jaël Theiler <e227484@edu.sbl.ch>
2024-04-12 14:48:58 +0200

    positionken

--- src/scenes/level-02.js
+++ src/scenes/level-02.js
@@ -1,5 +1,6 @@
 import { k, addGeneralGameLogic } from "../game.js"
 import { generateMapJumpAndRun } from "../map.js"
+
 import { loadKeyboardJumpAndRun } from "../keyboard.js"
 
 import "./level-03.js"
@@ -9,30 +10,45 @@ import "./level-03.js"
  *
  * Hier gibt es keine Gravitation, wir sind hier in einem RPG-Setting.
  */
-k.scene("level-02", async () => {
-  k.setGravity(0)
-  //createPlayer()
+k.scene(
+  "level-02",
+  async () => {
+    k.setGravity(0)
+    //createPlayer()
 
-  loadKeyboardJumpAndRun()
+    loadKeyboardJumpAndRun()
 
-  await generateMapJumpAndRun("maps/level-02.txt")
-  k.add([
-    k.sprite("background", { width: k.width(), height: k.height() }),
-    k.pos(0, 0),
-    k.fixed(),
-    k.z(-100),
-  ])
+    await generateMapJumpAndRun("maps/level-02.txt")
+    k.add([
+      k.sprite("background", { width: k.width(), height: k.height() }),
+      k.pos(0, 0),
+      k.fixed(),
+      k.z(-100),
+    ])
 
-  addGeneralGameLogic()
+    addGeneralGameLogic()
 
-  k.onCollide("player", "goal", (player) => {
-    {
-      k.go("level-03")
+    k.onCollide("player", "goal", (player) => {
+      {
+        k.go("level-03")
+      }
+    })
+
+    k.onCollide("player", "flower", (player, flower) => {
+      flower.destroy()
+      player.hasFlower = true
+    })
+  },
+  k.onUpdate(() => {
+    const player = k.get("player")[0]
+    if (player.pos.y > 720) {
+      k.go("lose")
     }
-  })
 
-  k.onCollide("player", "flower", (player, flower) => {
-    flower.destroy()
-    player.hasFlower = true
-  })
-})
+    // Wenn das Wurmloch links aus dem fenster geht, ist das spiel verloren
+    const wormhole = k.get("goal")[0]
+    if (wormhole.pos.x < 0) {
+      k.go("lose")
+    }
+  }),
+)
